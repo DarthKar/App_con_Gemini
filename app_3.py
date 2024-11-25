47555af3f09616ddde65a9efd8f8ec98f25428b7
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2024-11-25 17:09:05 -0500

    Update app_3.py

--- app_3.py
+++ app_3.py
@@ -1,7 +1,6 @@
-import csv
 import streamlit as st
 import pandas as pd
-import re
+import csv
 import io
 
 def extraer_datos(uploaded_file):
@@ -44,29 +43,13 @@ def extraer_datos(uploaded_file):
     df = pd.DataFrame(datos, columns=["Número de serie", "Nombre del producto", "Valor", "Día", "Mes", "Año", "Teléfono", "Email"])
     return df
 
-def generar_excel(df, nombre_archivo):
-    """Genera un archivo Excel a partir de un DataFrame.
-
-    Args:
-        df (pd.DataFrame): DataFrame con los datos.
-        nombre_archivo (str): Nombre del archivo Excel a generar.
-    """
-
-    df.to_excel(nombre_archivo, index=False)
-
 if __name__ == "__main__":
-    st.title("Generador de Excel a partir de CSV")
+    st.title("Visualizador de Datos CSV")
 
     # Cargar el archivo CSV
     uploaded_file = st.file_uploader("Selecciona el archivo CSV", type="csv")
     if uploaded_file is not None:
         df = extraer_datos(uploaded_file)
 
-        # Mostrar un preview del DataFrame
+        # Mostrar el DataFrame en una tabla interactiva
         st.dataframe(df)
-
-        # Generar el archivo Excel
-        if st.button("Generar Excel"):
-            nombre_archivo = "productos.xlsx"
-            generar_excel(df, nombre_archivo)
-            st.success(f"Archivo Excel generado: {nombre_archivo}")
